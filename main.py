da89ecc9c0f6cde27ecbb875d80b8131ec780b9b
Raphael <pusam.raphy@gmail.com>
2023-11-16 17:26:29 +0100

    basic routing functionality POST/GET with JSON

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# Test default route by returning JSON object
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'name': 'max', 'email': 'max@mustermann.com'})
+
+# POST route with body returning sent content
+@app.route('/', methods=['POST'])
+def return_content():
+    data = json.loads(request.data)
+    return jsonify(data)
+
+# not implemented routes
+@app.route('/', methods=['PUT'])
+def put_not_implemented():
+    return "<p>not implemented</p>"
+
+@app.route('/', methods=['DELETE'])
+def delete_not_implemented():
+    return "<p>not implemented</p>"
